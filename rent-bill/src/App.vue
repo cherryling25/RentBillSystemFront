0d949124bd9d0c1af8686a527145e1349a3242b8
Cherry <cherryling25@outlook.com>
2020-02-19 21:14:32 +0800

    home table

--- rent-bill/src/App.vue
+++ rent-bill/src/App.vue
@@ -15,6 +15,7 @@ export default {
 *{
   margin:0;
   padding: 0;
+  -webkit-font-smoothing: antialiased; 
 }
 
 </style>
--- rent-bill/src/components/room.vue
+++ rent-bill/src/components/room.vue
@@ -1,16 +1,71 @@
 <template>
   <div class="room">
-    <h1>{{ msg }}</h1>
+    <el-container>
+      <el-main>
+        <el-table :data="tableData" height="650">
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址">
+          </el-table-column>
+          <el-table-column fixed="right" label="操作" width="120">
+            <template slot-scope="scope">
+              <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">编辑</el-button>
+              <el-button @click.native.prevent="deleteRow(scope.$index, tableData)" type="text" size="small">
+                移除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
   name: 'room',
-  data() {
+    data() {
+      // const item = {
+      //   date: '2016-05-02',
+      //   name: '王小虎',
+      //   address: '上海市普陀区金沙江路 1518 弄'
+      // };
       return {
-        msg: 'room页面'
+        tableData: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-01',
+          name: 'ling',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-07',
+          name: 'jack',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }]
+        //tableData: Array(5).fill(item)
       }
+    },
+  methods:{
+    handleClick(row) {
+      console.log(row);
+    },
+    deleteRow(index, rows) {
+      rows.splice(index, 1);
+    }
   }
 }
 </script>
